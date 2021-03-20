df737245a15f8aacdc1caf492e67a5fdd1eca798
Jon Jagger <jon@jaggersoft.com>
2021-03-20 12:11:08 +0000

    Copy test data into saver service

--- sh/augmented_docker_compose.sh
+++ sh/augmented_docker_compose.sh
@@ -12,13 +12,8 @@ augmented_docker_compose()
 {
   cd "${ROOT_DIR}" && cat "./docker-compose.yml" \
     | docker run --rm --interactive cyberdojo/service-yaml \
-         custom-start-points \
-      exercises-start-points \
-      languages-start-points \
-                      runner \
                        model \
                        saver \
-                    selenium \
     | tee /tmp/augmented-docker-compose.creator.peek.yml \
     | docker-compose \
         --project-name cyber-dojo \

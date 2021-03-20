abcc8f24ebb8786d08ad9d09a9bc84362addae4c
Jon Jagger <jon@jaggersoft.com>
2021-03-20 09:34:30 +0000

    Dont build or run client when running just server tests

--- sh/build_tagged_images.sh
+++ sh/build_tagged_images.sh
@@ -5,24 +5,29 @@ source "${SH_DIR}/augmented_docker_compose.sh"
 #- - - - - - - - - - - - - - - - - - - - - - - -
 build_tagged_images()
 {
-  build_images
-  tag_images_to_latest
+  build_images "${@:-}"
+  tag_images_to_latest "${@:-}"
   check_embedded_env_var
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - - -
 build_images()
 {
+  if [ "${1:-}" == server ]; then
+    local -r target=downloader
+  fi	
   augmented_docker_compose \
     build \
-    --build-arg COMMIT_SHA=$(git_commit_sha)
+    --build-arg COMMIT_SHA=$(git_commit_sha) ${target:-}
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - - -
 tag_images_to_latest()
 {
   docker tag ${CYBER_DOJO_DOWNLOADER_IMAGE}:$(image_tag)        ${CYBER_DOJO_DOWNLOADER_IMAGE}:latest
-  docker tag ${CYBER_DOJO_DOWNLOADER_CLIENT_IMAGE}:$(image_tag) ${CYBER_DOJO_DOWNLOADER_CLIENT_IMAGE}:latest
+  if [ "${1:-}" != server ]; then
+    docker tag ${CYBER_DOJO_DOWNLOADER_CLIENT_IMAGE}:$(image_tag) ${CYBER_DOJO_DOWNLOADER_CLIENT_IMAGE}:latest
+  fi
   echo
   echo "echo CYBER_DOJO_DOWNLOADER_SHA=$(git_commit_sha)"
   echo "echo CYBER_DOJO_DOWNLOADER_TAG=$(image_tag)"
